b86519f28651e56fd748dc5caf31fd0a36be67ff
Lakshya Gupta <Aspirar@users.noreply.github.com>
2017-06-20 14:35:48 +0530

    Create 11727.cpp

---
+++ 11727.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    for (int i = 1; i <= t; i++) {
+        int a[3];
+        for (int j = 0; j < 3; j++) {
+            cin >> a[j];
+        }
+        sort(a, a + 3);
+        cout << "Case " << i << ": " << a[1] << endl;
+    }
+}
